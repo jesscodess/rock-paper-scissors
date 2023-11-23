f17fecdc17c47131eea5c8d7e6c069efc93d16aa
Jéssica Martins <jesscodes@proton.me>
2023-11-23 16:53:47 -0300

    Conclusão do projeto

--- script.js
+++ script.js
@@ -15,7 +15,16 @@ let buttons = document.querySelectorAll('button');
  * @return {String}   result result of the game
  */
 
-function calcResult(move1, move2) {}
+function calcResult(move1, move2) {
+  let result = move1 - move2;
+  if (result == 1 || result + 3 == 1) {
+    return winMsg;
+  } else if (result == 2 || result + 3 == 2) {
+    return loseMsg;
+  } else {
+    return tieMsg;
+  };
+}
 
 /**
  * @return {Number}   random number between 0 and 2
@@ -32,6 +41,7 @@ function startGame() {
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].textContent = moveList[i];
         buttons[i].style.display = "inline-block";
+        buttons[i].addEventListener("click", endGame);
       }
       moveDisplays.forEach(moveDisplay => moveDisplay.style.display='none');
 }
@@ -41,6 +51,24 @@ function startGame() {
  * @param {Event} event event containing information of user input.
  */
 
-function endGame(event) {}
+function endGame(event) {
+  let playerMove = moveList.indexOf(event.target.textContent);
+  let computerMove = randomMove();
+  statusDisplay.textContent = calcResult(playerMove, computerMove);
+  moveDisplays.forEach(
+    (moveDisplay) => (moveDisplay.style.display = "inline-block")
+  );
+  moveDisplays[0].textContent = `You played ${moveList[playerMove]}`;
+  moveDisplays[1].textContent = `Computer played ${moveList[computerMove]}`;
+  buttons.forEach((button, index) => {
+    if (index == 1) {
+      button.textContent = "Play Again";
+      button.removeEventListener("click", endGame);
+      button.addEventListener("click", startGame);
+    } else {
+      button.style.display = "none";
+    }
+  });
+}
 
 startGame();
